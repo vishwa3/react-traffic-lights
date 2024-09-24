55d1e2f9fdf18bca66c86638646b9b8dd4bd5d48
vishwa3 <vishwanathkrishnappa@gmail.com>
2024-09-24 16:46:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect /* useRef */ } from "react";
+import "./App.css";
 
-function App() {
+const config = {
+  red: {
+    duration: 4000,
+    next: "green",
+  },
+  yellow: {
+    duration: 500,
+    next: "red",
+  },
+  green: {
+    duration: 3000,
+    next: "yellow",
+  },
+};
+
+export default function App() {
+  // const intervalRef = useRef(null);
+  const [signalState, setSignalState] = useState("red");
+
+  useEffect(() => {
+    const { duration, next } = config[signalState];
+    const id = setTimeout(() => {
+      /* if (signalState < 3) {
+        console.log("if");
+        setSignalState(signalState + 1);
+      } else {
+        console.log("else");
+        setSignalState(1);
+      } */
+      /* setSignalState((prevSignalState) => {
+        if (prevSignalState === "red") return "green";
+        if (prevSignalState === "green") return "yellow";
+        return "red";
+      }); */
+      setSignalState(next);
+    }, duration);
+
+    return () => clearInterval(id);
+  }, [signalState]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app_container">
+      <div className="signal_container">
+        <div className={`signal ${signalState === "red" ? "red" : ""}`}></div>
+        <div
+          className={`signal ${signalState === "yellow" ? "yellow" : ""}`}
+        ></div>
+        <div
+          className={`signal ${signalState === "green" ? "green" : ""}`}
+        ></div>
+      </div>
     </div>
   );
 }
-
-export default App;
